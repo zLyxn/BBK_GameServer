f98ec3c1f0dc5c80bd7c4ba6057f9f74191d3e8a
Mika <79707824+zLyxn@users.noreply.github.com>
2025-02-18 08:15:52 +0100

    Add GameEngine class with initial game state structures

---
+++ src/main/scala/GameEngine.scala
@@ -0,0 +1,27 @@
+package org.bbk.gameserver
+
+class GameEngine {
+  enum Color {
+    case Red
+    case Blue
+    case Green
+    case Yellow
+    case Purple
+    case Orange
+    case Black
+    case White
+    case Grey
+    case None
+
+    def asString: String = this.toString
+  }
+
+  case class AbstractValue(value: Int, max: Int) //TODO change name
+
+  object Health extends AbstractValue(100, 100)
+  object ShieldHealth extends AbstractValue(100, 100)
+  object Energy extends AbstractValue(100, 100)
+  object ShipSpeed extends AbstractValue(100, 100)
+  var meteorAmount: Int = 0
+  var repairColor: Color = Color.None
+}
