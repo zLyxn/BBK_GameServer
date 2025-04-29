2ea5674c23a0d6343c3a612d3b9db8b784ace5a8
Jonathan <jonathan.uloth@gmail.com>
2025-04-29 09:23:23 +0200

    Add dynamic event loading and logging support in Events

--- src/main/scala/Events.scala
+++ src/main/scala/Events.scala
@@ -1,7 +1,40 @@
 package org.bbk.gameserver
 
+import com.typesafe.scalalogging.Logger
+
 object Events {
-  private val events: List[GameEvent] = List(ShieldDownEvent(), AttackEvent(), WeaponsbrokenEvent(), DriveBrokenEvent()) // TODO: Hier alle Events automatisch hinzufügen
+  //private val events_old: List[GameEvent] = List(ShieldDownEvent(), AttackEvent(), WeaponsbrokenEvent(), DriveBrokenEvent()) // TODO: Hier alle Events automatisch hinzufügen
+
+  var logger: Option[Logger] = None
+
+  private val events: List[GameEvent] = {
+    val packageName = "org.bbk.gameserver.events"
+    val excludedClasses = Set("GameEvent", "GameEventCompanion") // Falls bestimmte Klassen ausgeschlossen werden sollen
+    val runtimeMirror = scala.reflect.runtime.universe.runtimeMirror(getClass.getClassLoader)
+    val packageSymbol = runtimeMirror.staticPackage(packageName)
+
+    packageSymbol.info.decls
+      .filter(_.isClass)
+      .map(_.asClass)
+      .filterNot(cls => excludedClasses.contains(cls.name.toString))
+      .filter(cls => cls.name.toString.endsWith("Event"))
+      .flatMap { cls =>
+        try {
+          val clazz = runtimeMirror.runtimeClass(cls)
+          if (classOf[GameEvent].isAssignableFrom(clazz)) {
+            Some(clazz.getDeclaredConstructor().newInstance().asInstanceOf[GameEvent])
+          } else None
+        } catch {
+          case e: Exception =>
+            if logger.isDefined then logger.get.error("Error loading event class: " + cls.name + ": " + e.getMessage)
+            println("Error loading event class: " + cls.name + ": " + e.getMessage)
+            None
+        }
+      }
+      .toList
+  }
+  if logger.isDefined then logger.get.debug("Events loaded: " + events.map(_.getClass.getSimpleName).mkString(", "))
+  println("Debug: Events loaded: " + events.map(_.getClass.getSimpleName).mkString(", "))
 
   def getActiveEvents: List[GameEvent] = events.filter(_.isActive)
 
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,6 +4,7 @@ import com.typesafe.scalalogging.Logger
 @main def main(): Unit = {
   Thread().setName("GameServerThread-Main")
   val logger: Logger = Logger("GameServerLogger")
+  Events.logger = Some(logger)
   val server = new ConnectionEngine(Config.Connection.GAMEPORT, logger)
   val webServer = new WebServer(server, logger)
   webServer.start()
